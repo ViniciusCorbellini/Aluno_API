cb5eaf41d63ccbdfa56e6a2fb05c6362559de02d
ManoCorbas <corbellinivinicius4@gmail.com>
2025-02-26 12:08:58 -0300

    fix: variable name JSON bug

--- alunos/src/main/java/com/manocorbas/alunos/model/dtos/NotaDTO.java
+++ alunos/src/main/java/com/manocorbas/alunos/model/dtos/NotaDTO.java
@@ -2,4 +2,4 @@ package com.manocorbas.alunos.model.dtos;
 
 import com.manocorbas.alunos.model.domain.Aluno;
 
-public record NotaDTO(Aluno aluno, String nome, Float valor) {}
+public record NotaDTO(Long id_aluno, String nome, Float valor_nota) {}
