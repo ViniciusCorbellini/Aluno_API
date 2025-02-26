58cac70515fe2ec6ef88fff542530a7ced5a4763
ManoCorbas <corbellinivinicius4@gmail.com>
2025-02-26 12:10:05 -0300

    fix: Nota to NotaDTO

--- alunos/src/main/java/com/manocorbas/alunos/model/dtos/AlunoDTO.java
+++ alunos/src/main/java/com/manocorbas/alunos/model/dtos/AlunoDTO.java
@@ -4,5 +4,5 @@ import com.manocorbas.alunos.model.domain.Nota;
 
 import java.util.List;
 
-public record AlunoDTO(String nome, String matricula, List<Nota> notas) {
+public record AlunoDTO(String nome, String matricula, List<NotaDTO> notas) {
 }
