939d9f6986781a5e20142fbb4c1f6ac7a0815730
ManoCorbas <corbellinivinicius4@gmail.com>
2025-02-27 22:25:18 -0300

    refactor: threat404 exception handling

--- alunos/src/main/java/com/manocorbas/alunos/infra/exceptionhandlers/ControllerExceptionHandler.java
+++ alunos/src/main/java/com/manocorbas/alunos/infra/exceptionhandlers/ControllerExceptionHandler.java
@@ -3,6 +3,7 @@ package com.manocorbas.alunos.infra.exceptionhandlers;
 import com.manocorbas.alunos.model.dtos.ExceptionDTO;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,8 +17,9 @@ public class ControllerExceptionHandler {
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<String> threat404(EntityNotFoundException e){
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ExceptionDTO> threat404(EntityNotFoundException e){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(e.getMessage(), "404");
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionDTO);
     }
 
     @ExceptionHandler(Exception.class)
